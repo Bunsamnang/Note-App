9d8169ff5bdcc18b65ed57278554f10b1d6222ee
Bunsamnang <bunsamnangk@gmail.com>
2024-12-03 14:42:49 +0700

    testing

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import * as UserController from "../controllers/user";
-import { requiresAuth } from "../middleware/auth";
 
 const router = express.Router();
 
-router.get("/", requiresAuth, UserController.getAuthenticatedUser); // check if userId exist in the session first
+router.get("/", UserController.getAuthenticatedUser); // check if userId exist in the session first
 router.post("/signup", UserController.signUp);
 router.post("/login", UserController.login);
 router.post("/logout", UserController.logout);
