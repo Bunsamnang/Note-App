41272fa0ef7bd06dc0ee2672472676cde440a7d4
Bunsamnang <bunsamnangk@gmail.com>
2024-12-03 14:44:33 +0700

    testing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -40,6 +40,7 @@ app.use(
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
       secure: env.NODE_ENV === "production",
+      sameSite: "none", // Allow cross-origin cookies
     },
     rolling: true,
     store: MongoStore.create({
